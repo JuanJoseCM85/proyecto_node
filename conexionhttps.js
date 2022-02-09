2ee2facbed3f2d8c84e06260fddaf1a8f5a2e024
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-02-09 12:34:31 +0100

    Retos servidor HTTP

---
+++ conexionhttps.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+const server = http.createServer(function(request,response){
+    console.log("Peticion recibida del cliente");
+    console.log("URL: "+ request.url);
+    console.log("Método: " + request.method);
+    console.log("Cabecera Content-type:" + request.headers['content-type']);
+    console.log("Cabecera Content-length:" + request.headers['content-length']);
+    console.log("User-agent: "+ request.headers['user-agent']);
+    console.log("Response status code " + response.statusCode);
+    response.writeHead(200,{'Content-type': 'text/plain'});
+    
+    let web = request.url;
+    if(  web == "/bye"){
+        let mensaje = {
+            "statusCode": 200,
+            "message": {"ok": true,"message":"Adios!"}
+        }
+        response.write(JSON.stringify(mensaje));
+        console.log(mensaje);
+    }else{
+        let mensaje = {
+            "statusCode": 200,
+            "message": {"ok": true,"message":"Recibido!"}
+        }
+        response.write(JSON.stringify(mensaje));
+        console.log(mensaje);
+    }
+
+    response.end();
+});
+
+server.listen(3000);
---
+++ conexionhttpsexpress.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', function(req, res){
+    console.log("Petición recibida por el cliente");
+    console.log("Url: " + req.url);
+    console.log("Metodo : " + req.method);
+    console.log("User-agent: " + req.headers['user-agent']);
+
+    let mensaje = {
+        "statusCode": 200,
+        "message": {"ok": true,"message":"Recibido!"}
+    }
+
+        res.send(JSON.stringify(mensaje));
+
+});
+
+app.get('/bye', function(req, res){
+    console.log("Petición recibida por el cliente");
+    console.log("Url: " + req.url);
+    console.log("Metodo : " + req.method);
+    console.log("User-agent: " + req.headers['user-agent']);
+
+    let mensaje = {
+        "statusCode": 200,
+        "message": {"ok": true,"message":"Adios!"}
+    }
+
+        res.send(JSON.stringify(mensaje));
+
+});
+
+app.listen(7000);
